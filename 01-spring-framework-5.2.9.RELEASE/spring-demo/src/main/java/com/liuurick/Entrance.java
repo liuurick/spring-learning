5749940a3245319775ab31ef2cd4b47ec6f9a56f
liuurick <liuurick@gmail.com>
2020-10-19 22:46:47 +0800

    环境准备

---
+++ 01-spring-framework-5.2.9.RELEASE/spring-demo/src/main/java/com/liuurick/Entrance.java
@@ -0,0 +1,10 @@
+package com.liuurick;
+
+/**
+ * Created by liubin on 2020/10/19
+ */
+public class Entrance {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
